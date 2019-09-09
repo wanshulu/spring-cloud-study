4c0c01715655fdde5b8378448ac3006e2b2d3b1a
wanshulu <wanshulu@qq.com>
2019-09-09 22:08:11 +0800

    新项目

---
+++ microsevice-discovery-eureka-authenticating/src/main/java/com/sgcc/cloud/EurekaApplication.java
@@ -0,0 +1,14 @@
+package com.sgcc.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer//声明这是一个Eureka Server
+public class EurekaApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+}
